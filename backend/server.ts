9db08c3561a12689aff44cab698cb2f2b98413e8
mostafaelfawal <armostafa982@gmail.com>
2025-12-02 23:48:30 +0200

    init backend

---
+++ backend/server.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.listen(PORT, () =>
+  console.log(`the server is run http://localhost:${PORT}`)
+);
