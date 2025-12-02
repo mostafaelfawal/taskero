8b15df6994d5e0630a0f3c430c5bc93adbaad840
mostafaelfawal <armostafa982@gmail.com>
2025-12-03 00:11:26 +0200

    connect db

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import connectDB from "./config/db";
 
 const app = express();
 const PORT = process.env.PORT;
+
+connectDB();
 
 app.listen(PORT, () =>
   console.log(`the server is run http://localhost:${PORT}`)
