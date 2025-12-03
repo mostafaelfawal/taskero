7ce162581202d1cb0e876026e0cdc3e29fc1d00d
mostafaelfawal <armostafa982@gmail.com>
2025-12-03 05:11:50 +0200

    create auth UI

---
+++ frontend/app/auth/layout.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+import { ReactNode } from "react";
+import { FiCheck } from "react-icons/fi";
+
+export default function AuthLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex min-h-screen">
+      <div className="bg-gray-50 space-y-6 p-6 md:p-10 flex-1 flex flex-col justify-between">
+        <Link href="/auth/login" className="font-bold flex gap-2">
+          <div className="h-8 w-8 bg-violet-500 rounded-lg flex items-center justify-center shadow-lg shadow-violet-500/20">
+            <FiCheck color="white" />
+          </div>
+          <h1 className="text-xl">Taskero</h1>
+        </Link>
+        {children}
+        <div className="text-center md:text-left text-xs text-gray-600">
+          © 2025 Taskero . All rights reserved.
+        </div>
+      </div>
+
+      <div className="relative flex-1 hidden lg:block bg-linear-to-tl from-transparent via-transparent to-violet-200">
+        <Image
+          src="/authImage.png"
+          alt="auth image"
+          fill
+          className="object-cover opacity-40"
+        />
+
+        <div className="absolute bottom-0 flex flex-col justify-center p-12">
+          <h2 className="font-semibold text-xl max-w-md">
+            "Taskero has completely transformed how our team manages projects.
+            It's not just a tool, it's a new way of working."
+          </h2>
+
+          <footer className="text-sm text-gray-600 mt-4">
+            Mostafa Hamdi, Website builder
+          </footer>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/app/auth/login/page.tsx
@@ -0,0 +1,83 @@
+import Link from "next/link";
+import { BsGoogle } from "react-icons/bs";
+import { CiLock } from "react-icons/ci";
+import { FiArrowRight } from "react-icons/fi";
+import { MdOutlineMailOutline } from "react-icons/md";
+
+export default function Login() {
+  return (
+    <section className="flex flex-col justify-center w-full max-w-md mx-auto">
+      <h2 className="text-3xl text-center font-bold tracking-tight text-gray-900 font-display mb-2">
+        Welcome back
+      </h2>
+      <p className="text-gray-500 text-center font-semibold">
+        Enter your credentials to access your workspace.
+      </p>
+      <form className="space-y-3 my-7">
+        <div>
+          <label htmlFor="email" className="font-semibold ">
+            Email
+          </label>
+          <div className="relative mt-1">
+            <input
+              type="email"
+              id="email"
+              placeholder="name@example.com"
+              className="focus:outline-none focus:ring-2 focus:ring-violet-500 focus:border-transparent transition-shadow placeholder-gray-500 w-full rounded-xl border border-gray-200 shadow pl-10 py-2"
+            />
+            <MdOutlineMailOutline className="absolute top-3 left-3 text-gray-500" />
+          </div>
+        </div>
+        <div>
+          <div className="flex justify-between">
+            <label htmlFor="password" className="font-semibold ">
+              Password
+            </label>
+            <button
+              type="button"
+              className="text-sm text-violet-500 hover:underline"
+            >
+              Forget password?
+            </button>
+          </div>
+          <div className="relative mt-1">
+            <input
+              type="password"
+              id="password"
+              placeholder="••••••••"
+              className="focus:outline-none focus:ring-2 focus:ring-violet-500 focus:border-transparent transition-shadow placeholder-gray-500 w-full rounded-xl border border-gray-200 shadow pl-10 py-2"
+            />
+            <CiLock className="absolute top-3 left-3 text-gray-500" />
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="flex justify-center items-center gap-3 text-white bg-violet-500 font-semibold rounded-lg w-full py-2.5 hover:bg-violet-400 hover:scale-102 transition-all shadow-md shadow-violet-300"
+        >
+          Create account <FiArrowRight />
+        </button>
+        <div className="flex items-center w-full gap-3 my-6">
+          <div className="flex-1 h-px bg-gray-300"></div>
+
+          <p className="text-sm text-gray-500 whitespace-nowrap">
+            OR CONTINUE WITH
+          </p>
+
+          <div className="flex-1 h-px bg-gray-300"></div>
+        </div>
+        <button
+          type="button"
+          className="flex justify-center items-center gap-2 border border-gray-200 font-semibold rounded-lg w-full py-2.5 hover:bg-gray-100 transition-colors shadow-md"
+        >
+          <BsGoogle /> Google
+        </button>
+        <p className="text-center">
+          Don't have an account?{" "}
+          <Link href="/auth/signup" className="text-violet-500 hover:underline">
+            Sign up
+          </Link>
+        </p>
+      </form>
+    </section>
+  );
+}
